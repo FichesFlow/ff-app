69b62194eba66caa6d7d252fdce50145530ceb32
Mysterken <mysterken.dev@gmail.com>
2025-07-08 11:10:53 +0200

    add routing for decks with nested routes for creating and editing decks

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,43 @@
-import {createBrowserRouter} from "react-router";
-import Home from './pages/Home.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-import NotFound from './pages/NotFound.jsx';
-import Account from './pages/Account.jsx';
-import Leaderboard from './pages/Leaderboard.jsx';
-import TestPlayground from './pages/TestPlayground.jsx';
-import AddDeck from './pages/AddDeck.jsx';
-import Root from "./components/layout/Root.jsx";
-import Test from "./pages/Test.jsx";
+import {createBrowserRouter} from 'react-router'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import NotFound from './pages/NotFound.jsx'
+import Account from './pages/Account.jsx'
+import Leaderboard from './pages/Leaderboard.jsx'
+import TestPlayground from './pages/TestPlayground.jsx'
+import AddDeck from './pages/AddDeck.jsx'
+import Root from './components/layout/Root.jsx'
+import Test from './pages/Test.jsx'
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     Component: Root,
     children: [
       {index: true, Component: Home},
-      {path: "/account", Component: Account},
-      {path: "/leaderboard", Component: Leaderboard},
-      {path: "/playground", Component: TestPlayground},
-      {path: "/register", Component: Register},
-      {path: "/login", Component: Login},
-      {path: "/addDeck", Component: AddDeck},
-      {path: "*", Component: NotFound},
+
+      /* Pages générales */
+      {path: 'account', Component: Account},
+      {path: 'leaderboard', Component: Leaderboard},
+      {path: 'playground', Component: TestPlayground},
+      {path: 'register', Component: Register},
+      {path: 'login', Component: Login},
+      {path: 'test', Component: Test},
+
+      /* --------- Decks --------- */
+      {
+        path: 'decks',
+        children: [
+          {index: true, Component: NotFound /* TODO: DeckList */},
+          {path: 'new', Component: AddDeck},
+          {path: ':id', Component: NotFound /* TODO: DeckView */},
+          {path: ':id/edit', Component: AddDeck  /* TODO: DeckEdit  */}
+        ]
+      },
+
+      /* Fallback */
+      {path: '*', Component: NotFound}
     ]
   }
 ]);
