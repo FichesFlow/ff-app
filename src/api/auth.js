f8c33c2f80c343abdaf2ac7e47a264611e866c79
Mysterken <mysterken.dev@gmail.com>
2025-09-11 17:03:52 +0200

    fix new login route

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function login(email, password) {
   try {
-    const response = await axios.post(`${import.meta.env.VITE_API_URL}/login_check`, {
+    const response = await axios.post(`${import.meta.env.VITE_API_URL}/login`, {
       email,
       password
     });
